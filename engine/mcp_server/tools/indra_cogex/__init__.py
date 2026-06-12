f2434b0931bed6f481efea41f6466c70be71e530
guy <122138238+guy915@users.noreply.github.com>
2026-06-13 01:54:38 +0200

    Add Apache license headers to source files

--- engine/mcp_server/tools/indra_cogex/__init__.py
+++ engine/mcp_server/tools/indra_cogex/__init__.py
@@ -1,3 +1,17 @@
+# Copyright 2026 The Co-Scientist Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from .associations import query_gene_disease_network, query_gene_codependents
 from .drug_clinical import query_drug_info, query_clinical_trials
 from .pathways import query_pathways, query_causal_subnetwork
